7d401196510c08f9f12a8476dcd356182a4679b8
Khushal Das <khushaldasparmar@gmail.com>
2018-07-21 00:59:04 +0500

    Add files via upload
    These files include, C# code files, unity package and picture of project

---
+++ UIcontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIcontroller : MonoBehaviour {
+
+	public Button b1;
+	public Button b2;
+	public float i;
+	public Text paused;
+
+	// Use this for initialization
+	void Start () {
+		i = 0;
+		b1.onClick.AddListener (OnClickAction);
+		b2.onClick.AddListener (onClickAction2);
+		paused.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i == 0) {
+			Time.timeScale = 0.0f;
+		} else
+			Time.timeScale = 1.1f;
+	}
+
+	public void OnClickAction()
+	{
+		i = 1;
+		paused.text = "";
+	}
+	public void onClickAction2()
+	{
+		i = 0;
+		paused.text = "Paused";
+	}
+}
---
+++ playerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerControl : MonoBehaviour {
+	private Rigidbody rb;
+	private int count;
+	public Text countText; 
+	public Text winText;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		winText.text = "";
+
+	}
+		
+
+	void FixedUpdate () 
+
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement*10);
+
+	}
+	void OnTriggerEnter(Collider other ){
+		if (other.gameObject.CompareTag ("Pick up")) {
+			other.gameObject.SetActive (false); 
+			count = count + 1; 
+			countText.text = "Count: " + count.ToString ();
+		}
+		if (count >= 14) {
+			winText.text = "You are Winner.! ";
+		}
+	
+	}
+
+
+}
